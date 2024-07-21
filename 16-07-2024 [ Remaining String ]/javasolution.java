2f0c9308f9ff82e84ec5bc5375bbefb76c1d68b8
GOTIRUDRA <165698876+gotirudra@users.noreply.github.com>
2024-07-21 14:44:17 +0530

    Update javasolution.java

--- 16-07-2024 [ Remaining String ]/javasolution.java
+++ 16-07-2024 [ Remaining String ]/javasolution.java
@@ -1,11 +1,6 @@
-/*    "copy & past only code between lines (//----------//) for getting result"  */
-/*    "use online java compiler for getting output of whole code"      */
-
-//--------------------------------  start  ------------------------------
-
-public class Solution {
-
+class Solution {
     public String printString(String s, char ch, int count) {
+        // Your code goes here
         StringBuilder ans = new StringBuilder();
         
         for (int i = 0; i < s.length(); i++) {
@@ -14,23 +9,7 @@ public class Solution {
             }
             if (s.charAt(i) == ch) count--;
         }
-        
         return ans.toString();
-    } 
-}; // don't write this line while executing whole code
-
-//----------------------------  end  ----------------------------------------
-  
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-        
-        // Example input
-        String s = "abacabad";
-        char ch = 'a';
-        int count = 2;
-        
-        // Print the modified string
-        String result = sol.printString(s, ch, count);
-        System.out.println("Resulting string: " + result);
     }
 }
+
