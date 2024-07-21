e3d0b4c741c9598ac990ebb5ac93dc7dca7118ae
GOTIRUDRA <165698876+gotirudra@users.noreply.github.com>
2024-07-21 14:43:28 +0530

    Update c++solution.c

--- 15-07-2024 [ Smallest Number ]/c++solution.c
+++ 15-07-2024 [ Smallest Number ]/c++solution.c
@@ -1,30 +1,19 @@
 class Solution {
-    public String smallestNumber(int s, int d) {
-        // If the sum s is greater than the maximum possible sum with d digits, return "-1"
-        if (s > 9 * d) {
+  public:
+    string smallestNumber(int s, int d) {
+        if (9 * d < s) {
             return "-1";
         }
-
-        // Initialize the result as an empty string
-        String ans = "";
-
-        // Traverse the digits from the most significant to the least significant
-        for (int i = d; i >= 1; i--) {
-            // Find the maximum digit we can use for the current place
-            for (int j = 9; j >= 0; j--) {
-                // Check if the remaining sum after using j is valid
-                if (i != 1) {
-                    if (s - j > 0) {
-                        ans = j + ans;  // Append the digit to the result
-                        s -= j;         // Decrease the sum by the chosen digit
-                        break;
-                    }
-                } else {
-                    ans = s + ans;  // For the least significant digit, use the remaining sum
-                    break;
-                }
-            }
+        if (s > 9) {
+            return smallestNumber(s - 9, d - 1) + "9";
         }
-        return ans;
+        if (d == 1){
+            return to_string(s);
+        }
+        if (s > 1) {
+            return smallestNumber(1, d - 1) + to_string(s - 1);
+        }
+        
+        return smallestNumber(1, d - 1) + "0";
     }
 };
