bf944e49fa74dbea215724222fcd93388a88e81a
GOTIRUDRA <165698876+gotirudra@users.noreply.github.com>
2024-08-03 20:58:12 +0530

    Create javasolution.java

---
+++ 01-08-2024 To 30-08-2024/03-08-2024 [ The Celebrity Problem ]/javasolution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int celebrity(int[][] mat) {
+        int n = mat.length;
+        int candidate = 0;
+        
+        // Find a candidate
+        for (int i = 1; i < n; i++) {
+            if (mat[candidate][i] == 1) {
+                candidate = i;
+            }
+        }
+        
+        // Verify the candidate
+        for (int i = 0; i < n; i++) {
+            if (i != candidate && (mat[candidate][i] == 1 || mat[i][candidate] == 0)) {
+                return -1;
+            }
+        }
+        
+        return candidate;
+    }
+
+}
