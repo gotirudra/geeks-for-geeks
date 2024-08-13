4ba2ec081e4d54ec862484b67ab0fdf5e6cc83c3
GOTIRUDRA <165698876+gotirudra@users.noreply.github.com>
2024-08-13 14:43:34 +0530

    Create javasolution.java

---
+++ 01-08-2024 To 30-08-2024/13-08-2024 [ Square root of a number ]/javasolution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public long floorSqrt(long n) {
+        long s = 0, e = n;
+        long ans = 0;
+        while (s <= e) {
+            long mid = s + (e - s) / 2;
+            if (mid * mid == n) {
+                return mid;
+            } else if (mid * mid < n) {
+                ans = mid;
+                s = mid + 1;
+            } else {
+                e = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
