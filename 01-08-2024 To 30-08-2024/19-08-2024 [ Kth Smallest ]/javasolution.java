5ad6b2040133a8891d684af80747a2359a7f9c85
GOTIRUDRA <165698876+gotirudra@users.noreply.github.com>
2024-08-19 22:58:44 +0530

    Update javasolution.java

--- 01-08-2024 To 30-08-2024/19-08-2024 [ Kth Smallest ]/javasolution.java
+++ 01-08-2024 To 30-08-2024/19-08-2024 [ Kth Smallest ]/javasolution.java
@@ -1,41 +1,25 @@
+import java.util.*;
+
 class Solution {
-    public int kthSmallest(int[] arr, int l, int r, int k) {
-        return quickSelect(arr, l, r, k - 1); 
-    }
-    
-    private int quickSelect(int[] arr, int l, int r, int k) {
-        if (l == r) {
-            return arr[l];
+    public int kthSmallest(int[] arr, int k) {
+       
+        int maxi = Arrays.stream(arr).max().getAsInt();
+     
+        int[] freq = new int[maxi + 1];
+       
+        for (int num : arr) {
+            freq[num]++;
         }
-
-        int pivotIndex = partition(arr, l, r);
-        
-        if (k == pivotIndex) {
-            return arr[k];
-        } else if (k < pivotIndex) {
-            return quickSelect(arr, l, pivotIndex - 1, k);
-        } else {
-            return quickSelect(arr, pivotIndex + 1, r, k);
-        }
-    }
     
-    private int partition(int[] arr, int l, int r) {
-        int pivot = arr[r];
-        int i = l;
-
-        for (int j = l; j < r; j++) {
-            if (arr[j] <= pivot) {
-                swap(arr, i, j);
-                i++;
+        for (int i = 0; i <= maxi; i++) {
+            if (freq[i] != 0) {
+                k -= freq[i];
+                if (k <= 0) {
+                    return i;
+                }
             }
         }
-        swap(arr, i, r);
-        return i;
-    }
-
-    private void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+  
+        return -1;
     }
 }
